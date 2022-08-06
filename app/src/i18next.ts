9a2b9c0f4e2272351c71943c1ffa92128781ab8f
Mubarak Showole <mubarak.show@gmail.com>
2022-08-06 08:45:09 +0100

    prep app to receive translations from a backend

--- app/src/i18next.ts
+++ app/src/i18next.ts
@@ -1,5 +1,7 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./locales/en/translation.json";
 import fr from "./locales/fr/translation.json";
 
@@ -8,12 +10,27 @@ const resources = {
   fr,
 };
 
-i18next.use(initReactI18next).init({
-  resources,
-  lng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+      addPath: "/locales/{{lng}}/{{ns}}",
+      allowMultiLoading: false,
+      requestOptions: {
+        // used for fetch, can also be a function (payload) => ({ method: 'GET' })
+        mode: "cors",
+        credentials: "same-origin",
+        cache: "default",
+      },
+    },
+  });
 
 export default i18next;
